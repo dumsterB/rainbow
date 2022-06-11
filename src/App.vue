af55004c63c24c44fc7a6311410ec00398b1b265
Dumster <abduvalievbehr.uz1@gmail.com>
2022-06-11 11:20:36 +0500

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <v-app>
     <v-navigation-drawer app v-model="drawer" temporary>
-      <v-card
-          class="mx-auto"
-          max-width="300"
-          tile
-          elevation="0"
-      >
+      <v-card class="mx-auto" max-width="300" tile elevation="0">
         <v-list dense>
           <v-subheader>REPORTS</v-subheader>
-          <v-list-item-group
-              v-model="selectedItem"
-              color="primary"
-          >
+          <v-list-item-group v-model="selectedItem" color="primary">
             <v-list-item>
               <v-list-item-content>
                 <v-list-item-title></v-list-item-title>
@@ -23,77 +15,90 @@
       </v-card>
     </v-navigation-drawer>
 
-    <v-app-bar
-        app
-        style="background: white"
-        elevation="0"
-    >
-      <v-app-bar-nav-icon class="hidden-md-and-up" style="color: #00838F" @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar app style="background: white" elevation="0">
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        style="color: #00838f"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
 
       <v-toolbar-title>Health home</v-toolbar-title>
 
       <v-spacer></v-spacer>
-<div>
-      <v-btn  text class="links" v-scroll-to="'#aboutUs'">
-        {{$t("headerDefault.aboutUs")}}
-      </v-btn>
-     <v-btn  text class="links" v-scroll-to="'#services'">
-    {{$t("headerDefault.services")}}
-      </v-btn>
-</div>
+      <div>
+        <v-btn text class="links" v-scroll-to="'#aboutUs'">
+          {{ $t("headerDefault.aboutUs") }}
+        </v-btn>
+        <v-btn text class="links" v-scroll-to="'#services'">
+          {{ $t("headerDefault.services") }}
+        </v-btn>
+      </div>
 
       <div class="ml-2">
-        <v-btn v-if="currentLanguage==='ru'" @click="changeLanguage('uz')" dark style="background: transparent;"
-               elevation="0">
-          <country-flag style="margin-top: -4px" country='rus' size='normal'/>
+        <v-btn
+          v-if="currentLanguage === 'ru'"
+          @click="changeLanguage('uz')"
+          dark
+          style="background: transparent"
+          elevation="0"
+        >
+          <country-flag style="margin-top: -4px" country="rus" size="normal" />
         </v-btn>
-        <v-btn v-if="currentLanguage==='uz' " @click="changeLanguage('ru')" dark style="background: transparent"
-               elevation="0">
-          <country-flag style="margin-top: -4px" country='uz' size='normal'/>
+        <v-btn
+          v-if="currentLanguage === 'uz'"
+          @click="changeLanguage('ru')"
+          dark
+          style="background: transparent"
+          elevation="0"
+        >
+          <country-flag style="margin-top: -4px" country="uz" size="normal" />
         </v-btn>
       </div>
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
     <v-main>
-
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
         <AboutView id="aboutUs"></AboutView>
       </v-container>
     </v-main>
 
-    <v-footer app>
-    </v-footer>
+    <v-footer app> </v-footer>
   </v-app>
 </template>
 
 <script>
-import CountryFlag from 'vue-country-flag'
-import AboutView from './views/AboutView'
+import CountryFlag from "vue-country-flag";
+import AboutView from "./views/AboutView";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    CountryFlag,AboutView
+    CountryFlag,
+    AboutView,
   },
 
-  data(){
+  data() {
     return {
       selectedItem: 1,
       drawer: false,
-      currentLanguage: 'ru',
-      languages: [{val: 'ru', text: 'Русский-язык'}, {val: 'uz', text: 'Uzbek-tili'}],
-    }},
-  methods:{
+      currentLanguage: "ru",
+      languages: [
+        { val: "ru", text: "Русский-язык" },
+        { val: "uz", text: "Uzbek-tili" },
+      ],
+    };
+  },
+  methods: {
     changeLanguage(val) {
-      this.$i18n.locale = val
-      this.currentLanguage = val
-      console.log(val)
+      this.$i18n.locale = val;
+      this.currentLanguage = val;
+      console.log(val);
     },
     changeDrawer() {
-      this.drawer = !this.drawer
+      this.drawer = !this.drawer;
     },
-  }
+  },
 };
 </script>
 <style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import i18n from './plugins/i18n'
-import VueScrollTo from 'vue-scrollto'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import i18n from "./plugins/i18n";
+import VueScrollTo from "vue-scrollto";
 Vue.use(VueScrollTo, {
   container: "body",
   duration: 500,
@@ -16,11 +16,14 @@ Vue.use(VueScrollTo, {
   onDone: false,
   onCancel: false,
   x: false,
-  y: true
-})
-Vue.config.productionTip = false
+  y: true,
+});
+Vue.config.productionTip = false;
 
 new Vue({
-  router, store, vuetify,i18n,
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  store,
+  vuetify,
+  i18n,
+  render: (h) => h(App),
+}).$mount("#app");
